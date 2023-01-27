1d238a05c39250bba2ad75330f0158c5e7abfa73
icis-04 <chidexiwuaku@gmail.com>
2023-01-26 18:14:46 -0800

    initial commit

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" python script to check for uy=tf-8 validation"""
+
+
+def validUTF8(data):
+    """function to validate utf-8  characters"""
+    for i in range(0, len(data)):
+        numOfBytes = 0
+        val = data[i]
+        if val >= 255:
+            return False
+        elif val & 128 == 0:
+            numOfBytes = 1
+        elif val & 224 == 192:
+            numOfBytes = 2
+        elif val & 240 == 224:
+            numOfBytes = 3
+        elif val & 248 == 240:
+            numOfBytes = 4
+        else:
+            return False
+        j = 1
+        while j < numOfBytes:
+            if j + i > len(data):
+                return False
+            elif data[i + j] & 192 != 128:
+                return False
+            j += 1
+        i = i + numOfBytes - 1
+    return True
