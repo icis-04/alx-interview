42c14c1d9c601df3a30392d218a6e1383753570d
icis-04 <chidexiwuaku@gmail.com>
2023-02-09 14:20:48 -0800

    star wars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
 request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function (error, response, body) {
-	if (!error) {
-  const character = JSON.parse(body).characters;
-  printNames(character, 0);
-	}});
+  if (!error) {
+    const character = JSON.parse(body).characters;
+    printNames(character, 0);
+  }
+});
 function printNames (character, index) {
   request(character[index], function (error, response, body) {
     if (!error) {
