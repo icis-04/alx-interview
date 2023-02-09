bc60ece14ca1a342e325a5762ce9360b1f13276a
icis-04 <chidexiwuaku@gmail.com>
2023-02-09 14:13:09 -0800

    fixed order

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,10 +2,16 @@
 const request = require('request');
 request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function (error, response, body) {
   const character = JSON.parse(body).characters;
-	 for (let i = 0; i < character.length; i++) {
-    request(character[i], function (error, response, body) {
+  printNames(character, 0);
+});
+function printNames (character, index) {
+  request(character[index], function (error, response, body) {
+    if (!error) {
       console.log(JSON.parse(body).name);
     }
-    );
+    if (index + 1 < character.length) {
+      printNames(character, index + 1);
+    }
   }
-});
+  );
+}
