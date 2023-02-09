b1a43e3746a949096d6610ef8dc1be14e0ddca07
icis-04 <chidexiwuaku@gmail.com>
2023-02-09 14:19:40 -0800

    added to the file accounting for error

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function (error, response, body) {
+	if (!error) {
   const character = JSON.parse(body).characters;
   printNames(character, 0);
-});
+	}});
 function printNames (character, index) {
   request(character[index], function (error, response, body) {
     if (!error) {
