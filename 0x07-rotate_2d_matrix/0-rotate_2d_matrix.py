a7fa3cc214b6c1924b50d2d41104c7bd90182fd2
icis-04 <chidexiwuaku@gmail.com>
2023-02-16 06:57:01 +0000

    pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,7 +6,7 @@ def rotate_2d_matrix(matrix):
     """ function for rotating a square matrix"""
     for i in range(len(matrix)):
         for j in range(i, len(matrix)):
-            matrix[i][j],matrix[j][i] = matrix[j][i], matrix[i][j]
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
     N = len(matrix)
 
