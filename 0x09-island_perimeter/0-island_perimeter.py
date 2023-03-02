08ce59fbedd347e76d4d440a16631cd5966a6361
icis-04 <chidexiwuaku@gmail.com>
2023-03-02 23:49:46 +0000

    solved the dsa

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,10 +10,10 @@ def island_perimeter(grid):
     def dfs(i, j):
         """applying depth first search
         """
-        if i <= len(grid) or j <= len(grid[0]) or \
+        if i >= len(grid) or j >= len(grid[0]) or \
         i < 0 or j < 0 or grid[i][j] == 0:
             return 1
-        if grid[i][j] == 1:
+        if (i,j) in vector:
             return 0
 
         vector.add((i, j))
@@ -29,4 +29,4 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j]:
-               dfs(i, j)
+               return dfs(i, j)
