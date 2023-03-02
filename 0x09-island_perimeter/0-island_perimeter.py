f04a593bf50f889e5f33959e409faeb0df95adfe
icis-04 <chidexiwuaku@gmail.com>
2023-03-02 20:55:40 +0000

    placeholde code

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+""" Script to calculate the perimeter of an island """
+
+
+def island_perimeter(grid):
+    """ function to calculate the perimeter of the grid
+    """
+    vector = set() #To keep track of paths checked
+
+    def dfs(i, j):
+        """applying depth first search
+        """
+        if i <= len(grid) or j <= len(grid[0]) or \
+        i < 0 or j < 0 or grid[i][j] == 0:
+            return 1
+        if grid[i][j] == 1:
+            return 0
+
+        vector.add((i, j))
+
+        perim = dfs(i, j + 1)
+        perim += dfs(i + 1, j)
+        perim += dfs(i, j - 1)
+        perim += dfs(i - 1, j)
+
+        return perim
+
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+               dfs(i, j)
---
+++ 0x09-island_perimeter/README.md
@@ -0,0 +1,1 @@
+returns perimeter of an island
